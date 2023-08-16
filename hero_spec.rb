d15e583ebb2bea5fc72a6bf387350be1557f8a25
akmatkulov <akmatkulovs991@gmail.com>
2023-08-16 17:03:00 +0600

    Add RSpec tests for class Hero

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,4 +1,25 @@
 require './hero'
 
-describe Hero do 
+describe Hero do
+
+  it 'has a capitalized name' do
+    hero = Hero.new('mike')
+    expect(hero.name).to eq 'Mike'
+  end
+
+  it 'can power up' do
+    hero = Hero.new('mike')
+    expect(hero.power_up).to eq 110
+  end
+
+  it 'can power down' do
+    hero = Hero.new('mike')
+    expect(hero.power_down).to eq 90
+  end
+
+  it 'info hero' do
+    hero = Hero.new('mike')
+    expect(hero.hero_info).to eq 'Mike has a health 100'
+  end
+  
 end
